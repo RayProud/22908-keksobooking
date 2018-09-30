19ea3dcd9652350456be862925e07689163461a4
Прудников Роман Алексеевич <r.prudnikov@2gis.ru>
2018-09-30 23:42:08 +0300

    lint fixes

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-const VERSION = 'v0.0.1';
-const PROJECT_NAME = 'Кексобукинг';
+const VERSION = `v0.0.1`;
+const PROJECT_NAME = `Кексобукинг`;
 
 const cmd = process.argv[2];
 
 const CommandsName = {
-  help: '--help',
-  version: '--version',
+  help: `--help`,
+  version: `--version`,
 };
 
 const commands = {
   [CommandsName.help]: {
-    info: 'Доступные команды:\n--help    — печатает этот текст;\n--version — печатает версию приложения;',
-    execute: function() {
+    info: `Доступные команды:\n--help    — печатает этот текст;\n--version — печатает версию приложения;`,
+    execute() {
       console.log(this.info);
     }
   },
   [CommandsName.version]: {
     info: VERSION,
-    execute: function() {
+    execute() {
       console.log(this.info);
     }
   },
   default: {
     info: `Привет пользователь!\nЭта программа будет запускать сервер «${PROJECT_NAME}».\nАвтор: Кекс.`,
-    execute: function() {
+    execute() {
       console.log(this.info);
     }
   },
   error: {
     info: `Неизвестная команда ${cmd}.\nЧтобы прочитать правила использования приложения, наберите "--help"`,
-    execute: function() {
+    execute() {
       console.log(this.info);
     }
   },
